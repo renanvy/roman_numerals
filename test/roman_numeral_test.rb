5dc9c522b71e58ea7c90cd0dcaf5aa3f30fc3c79
Renan <renanvy@gmail.com>
2014-11-16 13:12:05 -0200

    Refactoring tests

--- test/roman_numeral_test.rb
+++ test/roman_numeral_test.rb
@@ -3,16 +3,22 @@ require_relative "../lib/roman_numeral"
 
 class RomanNumeralTest < Minitest::Test
 
-  def test_should_convert_symbol_I_to_number
+  def test_should_convert_one_symbol_to_number
     roman = RomanNumeral.new
     number = roman.convert("I")
     assert_equal(1, number)
   end
 
-  def test_should_convert_symbol_II_to_number
+  def test_should_convert_two_symbols_to_number
     roman = RomanNumeral.new
     number = roman.convert("II")
     assert_equal(2, number)
+  end
+
+  def test_should_understand_four_symbols
+    roman = RomanNumeral.new
+    number = roman.convert("XXII")
+    assert_equal(22, number)
   end
 
 end
